f5d03764ffa33983f2cc1de21baa3d7f0451ac7c
Summer Kate Nadela <summerkate.nadela@gmail.com>
2024-09-17 22:43:03 +0800

    Add logo

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function Details() {
   return (
     <div>
@@ -9,15 +9,27 @@ export default function Details() {
           <br />
           <br />
 
-          <b>Our Lady of Victories Church</b>
-          <br />
+          <a
+            href="https://maps.app.goo.gl/KVfH9yGwUXobqWNv5"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <b>Our Lady of Victories Church</b>
+            <br />
 
-          <span>2 Cannon, Quezon City, 1112 Metro Manila</span>
+            <span>2 Cannon, Quezon City, 1112 Metro Manila</span>
+          </a>
           <br />
           <br />
-          <b>Meyer's Place</b>
-          <br />
-          <span>Dapitan St, Quezon City, Metro Manila</span>
+          <a
+            href="https://maps.app.goo.gl/rBdccKnPVekYGqsQ9"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <b>Meyer's Place</b>
+            <br />
+            <span>Dapitan St, Quezon City, Metro Manila</span>
+          </a>
           <br />
           <br />
           <b>Formal Invitation to follow</b>
