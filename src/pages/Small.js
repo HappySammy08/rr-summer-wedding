226794b5568df33e3ce1f3a74db47b6dd4d36ae9
Summer Kate Nadela <68285562+HappySammy08@users.noreply.github.com>
2025-09-09 20:41:43 +0800

    Add files via upload

--- src/pages/Small.js
+++ src/pages/Small.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Divider from "../components/Divider";
 import Details from "../components/Details";
 import Calendar from "../components/CalendarSmall";
 
@@ -10,7 +9,6 @@ export default function Small() {
   return (
     <div className="Mobile">
       <Header />
-      <Divider />
       <Calendar />
       <Details />
       <Footer />
